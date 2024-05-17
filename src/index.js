0a50517a17757d9546d22221c7a89a95d685e5d6
h1alexbel <aliaksei.bialiauski@hey.com>
2024-05-17 18:51:17 +0300

    feat(#1): puzzles

--- src/index.js
+++ src/index.js
@@ -21,67 +21,70 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-
-import * as writer from "csv-writer";
-import minimist from "minimist";
-import fs from "fs";
-import {GraphQLClient} from "graphql-request";
+const minimist = require('minimist');
+const {GraphQLClient} = require('graphql-request');
+import {toJson} from './to-json.js';
+import {query as rq} from './ratelimit-query.js';
 
 const argv = minimist(process.argv.slice(2));
-const fileName = argv.filename || "results";
-const csv = writer.createObjectCsvWriter;
+const fileName = argv.filename || 'results';
+const batchsize = argv.batchsize || 10;
+const searchQuery = argv.query || '';
+const startDate = argv.start || '2008-01-01';
+const endDate = argv.end || now;
+const dateType = argv.date || 'created';
+// @todo #1:35min Add support for --tokens to pass file with tokens.
+//  We should add support for --tokens so, we users of the CLI will pass the
+//  file (filename) or an array that contains a few tokens. Don't forget to
+//  remove this puzzle.
+const tokens = argv.tokens || 'tokens';
 
-const MAXRESULTS = 1000; // This is the maximum number of results GitHub can provide from a query. If the query returns more than this number, the date range will be split into smaller batches.
-const REQUEST_TIMEOUT = 0; // Set the request timeout in milliseconds
+/**
+ * The maximum number of results GitHub can provide from a query.
+ * If the query returns more than this number, the date range will be split into smaller batches.
+ * @type {number}
+ */
+const MAXRESULTS = 1000;
+const TIMEOUT = 0;
 
-// Add --tokens support
-const tokens = [
-
-];
-
-let tokenIndex = 0;
-
-// Rotate through the available tokens and prevent rate limits or other restrictions that may be imposed on a single token.
-function getNextToken() {
-  const token = tokens[tokenIndex];
-  tokenIndex = (tokenIndex + 1) % tokens.length;
+let tindex = 0;
+function nextToken() {
+  const token = tokens[tindex];
+  tindex = (tindex + 1) % tokens.length;
   return token;
 }
 
-const batchSize = argv.batchsize || 10; // Set the desired batch size. Maximum is 100
-
-// Fecth a batch of results based on the provided search query, current date, cursor (pagination), and previous results.
+// @todo #1:60min Decompose fetch batch results into smaller pieces.
+//  We should decompose this large function into more manageable components in
+//  order to maintain it in the future. Let's create a few unit test as well.
+//  Don't forget to remove this puzzle.
 async function fetchResultsBatch(searchQuery, currentDate, cursor = null, results = []) {
   try {
-    const client = new GraphQLClient("https://api.github.com/graphql", {
+    const client = new GraphQLClient('https://api.github.com/graphql', {
       headers: {
-        Authorization: `Bearer ${getNextToken()}`
+        Authorization: `Bearer ${nextToken()}`
       }
     });
-
     const data = await client.request(query, {
       searchQuery,
-      first: batchSize,
+      first: batchsize,
       after: cursor
     });
-
-    const { nodes, pageInfo } = data.search;
+    const {nodes, pageInfo} = data.search;
     results.push(...nodes);
     if (currentDate !== undefined) {
       console.log(`\nExtracted ${results.length} results for ${currentDate}...\n\n`);
     } else {
       console.log(`\nExtracted ${results.length} results so far...`);
     }
-
-    const rateLimitData = await client.request(rateLimitQuery);
+    const rateLimitData = await client.request(rq);
     const rateLimit = rateLimitData.rateLimit;
-    console.log("Rate Limit:", rateLimit);
-    console.log("hasNextPage:", pageInfo.hasNextPage);
-    console.log("endCursor:", pageInfo.endCursor);
-
+    console.log('Rate Limit:', rateLimit);
+    console.log('hasNextPage:', pageInfo.hasNextPage);
+    console.log('endCursor:', pageInfo.endCursor);
     if (pageInfo.hasNextPage) {
       // Delay between batches to avoid rate limits
-      await new Promise((resolve) => setTimeout(resolve, REQUEST_TIMEOUT)); // Adjust the delay time as needed
+      await new Promise((resolve) => setTimeout(resolve, TIMEOUT)); // Adjust the delay time as needed
       return await fetchResultsBatch(searchQuery, currentDate, pageInfo.endCursor, results);
     } else {
       return results;
@@ -91,17 +94,22 @@ async function fetchResultsBatch(searchQuery, currentDate, cursor = null, result
   }
 }
 
+// @todo #1:45min Create integration test case with GitHub GraphQL API.
+//  We should create integration test cases for GitHub GraphQL API.
+//  Let's try to use open repositories without PAT passing. Besides the
+//  test case, let's move this function into `ranged.js`. Don't forget to
+//  remove this puzzle.
 async function resultsInDateRange(completeSearchQuery) {
-  console.log("Checking if date range should be split: " + completeSearchQuery);
+  console.log('Checking if date range should be split: ' + completeSearchQuery);
   try {
-    const client = new GraphQLClient("https://api.github.com/graphql", {
+    const client = new GraphQLClient('https://api.github.com/graphql', {
       headers: {
-        Authorization: `Bearer ${getNextToken()}`
+        Authorization: `Bearer ${nextToken()}`
       }
     });
-    let data = await client.request(countQuery, { completeSearchQuery });
+    let data = await client.request(countQuery, {completeSearchQuery});
     console.log(data);
-    const { repositoryCount } = data.search;
+    const {repositoryCount} = data.search;
     console.log(`Results: ${repositoryCount}`);
     return repositoryCount;
   } catch (error) {
@@ -112,69 +120,61 @@ async function resultsInDateRange(completeSearchQuery) {
 
 
 // Determine whether to fetch all results in a single batch or in multiple batches based on total results and dates.
+// @todo #1:45min Decompose fetchAllResults function into more manageable components.
+//  We should decompose that large function into smaller pieces that we can
+//  easily change or refactor. Let's create a few unit tests as well. Don't
+//  forget to remove this puzzle.
 async function fetchAllResults() {
   try {
-    const client = new GraphQLClient("https://api.github.com/graphql", {
+    const client = new GraphQLClient('https://api.github.com/graphql', {
       headers: {
-        Authorization: `Bearer ${getNextToken()}`
+        Authorization: `Bearer ${nextToken()}`
       }
     });
-
-    const data = await client.request(countQuery, { completeSearchQuery });
-    const { repositoryCount } = data.search;
+    const data = await client.request(countQuery, {completeSearchQuery: compiled});
+    const {repositoryCount} = data.search;
     console.log(`Total results: ${repositoryCount}`);
-
     if (repositoryCount <= MAXRESULTS) {
-      return fetchResultsBatch(completeSearchQuery);
+      return fetchResultsBatch(compiled);
     } else {
       let startDateObj = new Date(startDate);
       let endDateObj = new Date(endDate);
-
       const dayInMilliseconds = 24 * 60 * 60 * 1000;
       let dayCount = Math.ceil((endDateObj - startDateObj) / dayInMilliseconds);
-      //    console.log(dayCount);
       let results = [];
-
-      //keep dividing the search query into smaller batches based on the date range
       let currentStartDateObj = startDateObj;
       let currentStartDate = startDate;
       let nextEndDateObj = new Date(currentStartDateObj.getTime() + dayInMilliseconds * dayCount / 2);
-      let nextEndDate = nextEndDateObj.toISOString().split("T")[0];
-
+      let nextEndDate = nextEndDateObj.toISOString().split('T')[0];
       while (nextEndDate <= endDate) {
         let nextSearchQuery = `${searchQuery} ${dateType}:${currentStartDate}..${nextEndDate}`;
         let nextResultCount = await resultsInDateRange(nextSearchQuery);
         if (nextResultCount === null) {
-          console.log("Error: No results found.");
+          console.log('Error: No results found.');
           return null;
         }
         if (nextResultCount <= MAXRESULTS) {
-
           if (nextResultCount > 0) {
-            let result = await fetchResultsBatch(nextSearchQuery, currentStartDate + ".." + nextEndDate);
+            let result = await fetchResultsBatch(nextSearchQuery, currentStartDate + '..' + nextEndDate);
             results.push(...result);
           }
-
           console.log(`\nExtracted ${results.length} results for ${currentStartDate}..${nextEndDate}...`);
-
           currentStartDateObj = new Date(nextEndDateObj.getTime() + dayInMilliseconds);
-          currentStartDate = currentStartDateObj.toISOString().split("T")[0];
+          currentStartDate = currentStartDateObj.toISOString().split('T')[0];
           if (currentStartDate > endDate) break;
           nextEndDateObj = endDateObj;
-          nextEndDate = nextEndDateObj.toISOString().split("T")[0];
-        }
-        else {
+          nextEndDate = nextEndDateObj.toISOString().split('T')[0];
+        } else {
           dayCount = Math.ceil((nextEndDateObj - currentStartDateObj) / dayInMilliseconds);
-          //console.log(`\nSplitting ${currentStartDate}..${nextEndDate} into ${dayCount} days...`);
-          if (dayCount == 1)
+          console.log(`\nSplitting ${currentStartDate}..${nextEndDate} into ${dayCount} days...`);
+          if (dayCount == 1) {
             nextEndDateObj = new Date(currentStartDateObj.getTime());
-          else
+          } else {
             nextEndDateObj = new Date(currentStartDateObj.getTime() + dayInMilliseconds * dayCount / 2);
-
-          nextEndDate = nextEndDateObj.toISOString().split("T")[0];
+          }
+          nextEndDate = nextEndDateObj.toISOString().split('T')[0];
         }
       }
-
       return results;
     }
   } catch (error) {
@@ -182,16 +182,20 @@ async function fetchAllResults() {
   }
 }
 
-// Write formatted data in JSON and CSV files
+// @todo #1:45min Add support for dynamic field mapping.
+//  Let's add support for dynamic field mapping, user will define his own
+//  mapping in `.yml` file, we will parse that and apply it when writing
+//  results to the files. e.g.: name: result.nameWithOwner, etc. In this
+//  case `result` should be bindable only in that `.yml` config.
 function writeFiles(json) {
   const formattedResults = json.map((result) => {
     // Modify according to the desired format and extraction fields
     const data = {
-      name: result.nameWithOwner.split("/")[1],
-      owner: result.nameWithOwner.split("/")[0],
-      description: result.description ? result.description : "",
+      name: result.nameWithOwner.split('/')[1],
+      owner: result.nameWithOwner.split('/')[0],
+      description: result.description ? result.description : '',
       url: result.url,
-      createdAt: result.createdAt.split("T")[0],
+      createdAt: result.createdAt.split('T')[0],
       // users: result.assignableUsers.totalCount,
       // watchers: result.watchers.totalCount,
       stars: result.stargazerCount,
@@ -200,74 +204,18 @@ function writeFiles(json) {
       issues: result.issues.totalCount,
       pullRequests: result.pullRequests.totalCount,
       diskUsage: result.diskUsage,
-      license: result.licenseInfo ? result.licenseInfo.spdxId : "",
+      license: result.licenseInfo ? result.licenseInfo.spdxId : '',
       languages: result.languages.edges.map((edge) => edge.node.name),
-      primaryLanguage: result.primaryLanguage ? result.primaryLanguage.name : "",
+      primaryLanguage: result.primaryLanguage ? result.primaryLanguage.name : '',
       environments: result.environments.edges.map((edge) => edge.node.name),
       submodules: result.submodules.edges.map((edge) => edge.node.name),
       topics: result.repositoryTopics.edges.map((edge) => edge.node.topic.name),
     };
-
-    // Check if the dictionary file exists
-    if (fs.existsSync("dictionary.json")) {
-      // Read the dictionary file
-      const dictionary = require("./dictionary.json");
-
-      // Escape special characters in the tag for regular expression
-      const escapeRegExp = (string) => {
-        return string.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
-      };
-
-      // Check if any word in the name or description matches the tag from the dictionary
-      const isTagInNameOrDescription = (tag) => {
-        const regexString = `\\b${escapeRegExp(tag)}\\b`;
-        const regex = new RegExp(regexString, "i");
-
-        // Check if the tag is found as a whole word in the name or description
-        return regex.test(data.name) || regex.test(data.description);
-      };
-
-      // Filter the dictionary tags based on word matching
-      const nameAndDescTags = dictionary.filter((currentTag) => isTagInNameOrDescription(currentTag.tag));
-
-      // Check if the tags are already in the topics field, and if not, add them to the 'extra' column
-      const extraTags = nameAndDescTags.filter((currentTag) => {
-        return !data.topics.includes(currentTag.tag);
-      });
-
-      data.extra = extraTags.map((currentTag) => currentTag.tag);
-    }
-
     return data;
   });
-
-
-  // Save as JSON
-  fs.writeFile(`${fileName}.json`, JSON.stringify(formattedResults, null, 2), function (err) {
-    if (err) throw err;
-    console.log(`${fileName}.json file saved`);
-  });
-
-  // Save as CSV
-  const csvWriter = csv({
-    path: `${fileName}.csv`,
-    header: Object.keys(formattedResults[0]).map((key) => ({ id: key, title: key })),
-  });
-
-  csvWriter
-    .writeRecords(formattedResults)
-    .then(() => console.log(`${fileName}.csv file saved`))
-    .catch((err) => console.error(err));
+  toJson(fileName, formattedResults);
+  toCsv(fileName, formattedResults);
 }
-
-
-// Write JSON file without any format or dictionary
-// function writeJsonFile(json) {
-//   fs.writeFile(`${fileName}.json`, JSON.stringify(json, null, 2), function (err) {
-//     if (err) throw err;
-//     console.log(`${fileName} file saved`);
-//   });
-// }
 
 // Set query, count its totals and check rate limits
 const query = `query ($searchQuery: String!, $first: Int, $after: String) {
@@ -342,27 +290,8 @@ const countQuery = `query ($completeSearchQuery: String!) {
   }
 }`;
 
-const rateLimitQuery = `query {
-  rateLimit {
-    limit
-    cost
-    remaining
-    used
-    resetAt
-    nodeCount
-  }
-}`;
-
-// Create arguments and set defaults
-const now = new Date().toISOString().split("T")[0];
-const searchQuery = argv.query || "";
-const startDate = argv.start || "2008-01-01";
-const endDate = argv.end || now;
-const dateType = argv.date || "created";
-
-// Construct the search query with the date range
-const completeSearchQuery = `${searchQuery} ${dateType}:${startDate}..${endDate}`;
-console.log("Search Query:", completeSearchQuery);
+const compiled = `${searchQuery} ${dateType}:${startDate}..${endDate}`;
+console.log('Compiled search query:', compiled);
 
 // Run and write the extraction
 fetchAllResults()
